5df70b6ce674436c24272f439597ec681bf4e091
sortzis <sortzis@mail.nmc.edu>
2022-12-06 22:31:51 -0500

    chapter 11

---
+++ Models/ClientService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RazorPages.Models
+{
+    public class ClientService
+    {
+        [Key]
+        public int ID { get; set; }
+        [ForeignKey("ClientID")]
+        public int ClientID { get; set; }
+        public Client Client { get; set; }
+        [ForeignKey("ServicesID")]
+        public int ServicesID { get; set; }
+        public Services Services { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime Date { get; set; }
+    }
+}
